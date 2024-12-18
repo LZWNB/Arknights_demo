02352a50a647ae399e302c6fafaed4bf5989a673
LZWNB <2788267031@qq.com>
2024-12-18 14:21:38 +0800

    feat: 更新文本信息

--- .history/src/commponents/contact/Contact_20241020232815.jsx
+++ .history/src/commponents/contact/Contact_20241020232815.jsx
@@ -45,7 +45,7 @@ export const Contact = () => {
         <motion.h1 variants={variants}>Let’s work together</motion.h1>
         <motion.div className="item" variants={variants}>
           <h2>Mail</h2>
-          <span>hello@react.dev</span>
+          <span>2788267031@qq.com</span>
         </motion.div>
         <motion.div className="item" variants={variants}>
           <h2>Address</h2>
