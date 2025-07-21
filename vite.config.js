93b9db8400882628f8d0b2786b9e7a7f57b0af1c
lzw_plus <2678645993@qq.com>
2025-07-21 18:04:03 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import styles from 'rollup-plugin-styles'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Arknights_demo/',
-  plugins: [
-    react(),
-    styles({
-      // 设置为 true 以注入 CSS 到 JavaScript 中
-      inject: true,
-      // 设置为 true 以将 CSS 文件输出到单独的文件中
-      // output: 'bundle.css',
-      // 设置为 true 以启用 CSS Modules
-      modules: false,
-      // 设置为 true 以启用 PostCSS
-      postcss: false,
-      // 设置为 true 以启用 SASS/SCSS
-      sass: true,
-    }),
-  ],
+  plugins: [react()],
   css: {
     modules: {
-      localsConvention: 'dashes'
+      localsConvention: 'dashes',
+      // 添加生成类名的配置
+      generateScopedName: '[name]__[local]___[hash:base64:5]'
     },
     preprocessorOptions: {
       scss: {
-        // 你可以在这里添加SCSS全局变量或混合
+        // 添加全局 Sass 文件导入
       }
     }
   },
   build: {
     outDir: 'docs'
   },
+  resolve: {
+    alias: {
+      // 添加路径别名方便引用
+    }
+  }
 })
