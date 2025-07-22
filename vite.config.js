ab66fa9203a2bd4b73c5c09410f173cd07db53c1
lzw_plus <2678645993@qq.com>
2025-07-22 09:11:15 +0800

    test: test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  base: '/',
+  base: '/Arknights_demo/',
   plugins: [react()],
   css: {
     modules: {
