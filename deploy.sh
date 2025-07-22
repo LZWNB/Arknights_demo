f3dffe7d95d1939e18ed5a52176267f541d3927e
lzw_plus <2678645993@qq.com>
2025-07-22 09:22:54 +0800

    fix: test

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:lzwnb/Arknights_demo.git main:gh-pages
 
 cd -
